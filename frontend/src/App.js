4b548e673f7c53359e8e32ccf2096cbdd23ea3d0
Mostafa Abdelsalam <abdelslam.mostafa@gmail.com>
2021-01-13 23:37:17 +0200

    adding product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,33 @@
 import React from 'react';
-import data from './data';
-import Product from './Product';
+import {BrowserRouter as Router , Route} from 'react-router-dom';
+import  HomeScreen from "./HomeScreen";
+import  ProductScreen from "./ProductScreen";
+
+
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">
-            amazona
-          </a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div>
-          <div className="row center">
-            {data.products.map((product) => (
-              <Product key={product._id} product={product}></Product>
-            ))}
+    <Router>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              amazona
+            </a>
           </div>
-        </div>
-      </main>
-      <footer className="row center">All right reserved</footer>
-    </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/product/:id" exact component={ProductScreen}></Route>
+          <Route path="/" exact component={HomeScreen}></Route>
+          
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
+    </Router>
+    
   );
 }
 
